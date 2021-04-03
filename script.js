d9361daa21bf62dd7376b1ddf224cdd1df239220
Abhiram-932 <77227232+Abhiram-932@users.noreply.github.com>
2021-04-03 19:49:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ request.open("GET","https://restcountries.eu/rest/v2/all",true);
 request.send();
 request.onload=function(){
     var data=JSON.parse(this.response);
-    for(i=0;i<=data.length;i++){
-        console.log(data[i].name);
-        }
-
+    var total=data.reduce((acc,country)=>{
+        return acc+country.population;
+    }, 0);
+    console.log(total);
 }
